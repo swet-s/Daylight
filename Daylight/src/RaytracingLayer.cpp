69e952383c382311d28b3680f2c319bcc931ae6b
s-swet <shreet.swet@gmail.com>
2022-08-23 03:33:17 +0530

    Added Raytracer class

--- Daylight/src/RaytracingLayer.cpp
+++ Daylight/src/RaytracingLayer.cpp
@@ -1,1 +1,46 @@
 #include "RaytracingLayer.h"
+
+void RaytracingLayer::OnUIRender()
+{
+	ImGui::Begin("Settings");
+
+	if (ImGui::Button("Render"))
+	{
+		Render();
+	}
+
+	ImGui::Text("Last render: %.3fms", m_LastRenderTime);
+
+	ImGui::End();
+
+	// Creating Viewport
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	ImGui::Begin("Viewport");
+
+	m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+	m_ViewportHeight = ImGui::GetContentRegionAvail().y; 
+	
+	auto image = m_Raytracer.GetFinalImage();
+	if (image)
+	{
+		// Deploying the image
+		ImVec2 size = { (float)image->GetWidth(), (float)image->GetHeight() };
+		ImGui::Image(image->GetDescriptorSet(), size, ImVec2(0, 1), ImVec2(1, 0));
+	}
+
+	ImGui::End();
+	ImGui::PopStyleVar();
+
+	//Rendering every frame
+	Render();
+}
+
+void RaytracingLayer::Render()
+{
+	Walnut::Timer timer;
+
+	m_Raytracer.OnResize(m_ViewportWidth, m_ViewportHeight);
+	m_Raytracer.Render();
+
+	m_LastRenderTime = timer.ElapsedMillis();
+}
--- Daylight/src/RaytracingLayer.h
+++ Daylight/src/RaytracingLayer.h
@@ -5,36 +5,16 @@
 #include "Walnut/Image.h"
 #include "Walnut/Timer.h"
 
+#include "Raytracer.h"
+
 class RaytracingLayer : public Walnut::Layer
 {
 public:
+	virtual void OnUIRender() override;
 
-	virtual void OnUIRender() override
-	{
-
-		ImGui::Begin("Settings");
-
-		ImGui::Text("Last render: %.3fms", m_LastRenderTime);
-
-		ImGui::End();
-
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-		ImGui::Begin("Viewport");
-
-		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
-		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
-
-		ImGui::End();
-		ImGui::PopStyleVar();
-
-		//Walnut::Timer timer;
-
-
-		//m_LastRenderTime = timer.ElapsedMillis();
-	}
-
+	void Render();
 private:
-	//Renderer m_Renderer;
+	Raytracer m_Raytracer;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 
 	float m_LastRenderTime = 0.0f;
